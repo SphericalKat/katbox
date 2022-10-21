3c0256f51eff233bc477470b1d9f59929b319db1
Amogh Lele <amolele@gmail.com>
2022-10-22 00:50:07 +0530

    feat(frontend): implement progress bar logic
    Signed-off-by: Amogh Lele <amolele@gmail.com>

--- frontend/src/upload.ts
+++ frontend/src/upload.ts
@@ -3,6 +3,9 @@ import axios from "axios";
 const form: HTMLFormElement = document.getElementById(
   "upload"
 ) as HTMLFormElement;
+const progressbar = document.getElementById("progressbar");
+const progress = document.getElementById("progress");
+const submitBtn = document.getElementById("submit");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -19,12 +22,18 @@ form.addEventListener("submit", (e) => {
       "Content-Type": "multipart/form-data",
     },
     onUploadProgress: e => {
-        console.log(e)
+        progressbar.style.display = 'block';
+        const percent = Math.round((e.loaded * 100) / e.total)
+        progress.style.width = `${percent}%`;
+        submitBtn.style.display = 'none'
     }
   }).then(_res => {
     const banner = document.getElementById("banner")
     banner.style.display = 'block';
     banner.style.backgroundColor = 'green';
     banner.textContent = "Upload complete."
+
+    submitBtn.style.display = 'block';
+    progressbar.style.display = 'none';
   })
 });
